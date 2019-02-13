967396f0ca2c9294d779131bd5b9114498130455
LoreMiranda <lorena.miranda.lagos@gmail.com>
2019-02-13 07:07:03 -0300

    Se agrega boton de limpiar funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,33 @@
 window.cipher = {
-  encode: (fraseInicial, key) => { //le damos los parámetros a considerar
-      let texto = ""; //y que vaya guardando todo en esta variable vacía
-      for (let i = 0; i < fraseInicial.length; i++) { //desde la posición 0 recorra 1 a 1 los caracteres
-          let sdoTexto = fraseInicial.charCodeAt(i); //la varible "sdoTexto" almacena el texto en ascii
-          if (sdoTexto >= 65 && sdoTexto <= 90) { //le damos condición para que solo se mueva entre las 26 letras del abc
-              let code = (sdoTexto - 65 + key) % 26 + 65; //se aplica fórmula de Michelle y que se guarde en la variable "code"
-              texto += String.fromCharCode(code); //que nos devuelva el string en ascii aplicando el offset 
+  encode: (firstText, key) => { //le damos los parámetros a considerar
+      let finalText = ""; //y que vaya guardando todo en esta variable vacía
+      for (let i = 0; i < firstText.length; i++) { //desde la posición 0 recorra 1 a 1 los caracteres
+          let secondText = firstText.charCodeAt(i); //la varible "secondText" almacena el texto en ascii
+          if (secondText >= 65 && secondText <= 90) { //le damos condición para que solo se mueva entre las 26 letras del abc
+              let code = (secondText - 65 + key) % 26 + 65; //se aplica fórmula de Michelle y que se guarde en la variable "code"
+              finalText += String.fromCharCode(code); //que nos devuelva el string en ascii aplicando el offset 
           } else {
-              texto += String.fromCharCode(sdoTexto); //que pase de largo los caracteres que no se encuentran entre las posiciones 65 a 90 de ascii
+              finalText += String.fromCharCode(secondText); //que pase de largo los caracteres que no se encuentran entre las posiciones 65 a 90 de ascii
           }
       }
-      return texto;
+      return finalText;
   },
-  decode: (fraseInicial, key) => { //le damos los parámetros a considerar
-      let texto = ""; //y que vaya guardando todo en esta variable vacía
-      for (let i = 0; i < fraseInicial.length; i++) { //desde la posición 0 recorra 1 a 1 los caracteres
-          let sdoTexto = fraseInicial.charCodeAt(i); //la varible "sdoTexto" almacena el texto en ascii
-          if (sdoTexto >= 65 && sdoTexto <= 90) { //le damos condición para que solo se mueva entre las 26 letras del abc
-              let code = (sdoTexto + 65 - key) % 26 + 65; //se aplica fórmula de Michelle y que se guarde en la variable "code"
-              texto += String.fromCharCode(code); //que nos devuelva el string en ascii aplicando el offset
+  decode: (firstText, key) => { //le damos los parámetros a considerar
+      let finalText = ""; //y que vaya guardando todo en esta variable vacía
+      for (let i = 0; i < firstText.length; i++) { //desde la posición 0 recorra 1 a 1 los caracteres
+          let secondText = firstText.charCodeAt(i); //la varible "secondText" almacena el texto en ascii
+          if (secondText >= 65 && secondText <= 90) { //le damos condición para que solo se mueva entre las 26 letras del abc
+              let code = (secondText + 65 - key) % 26 + 65; //se aplica fórmula de Michelle y que se guarde en la variable "code"
+              finalText += String.fromCharCode(code); //que nos devuelva el string en ascii aplicando el offset
           } else {
-              texto += String.fromCharCode(sdoTexto); //que pase de largo los caracteres que no se encuentran entre las posiciones 65 a 90 de ascii
+              finalText += String.fromCharCode(secondText); //que pase de largo los caracteres que no se encuentran entre las posiciones 65 a 90 de ascii
           }
       }
-      return texto;
+      return finalText;
   }
+
+
+
+
+
 };
--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-/* le decimos al botón "codificar" que al hacer click guarde en la variable "encrypText" lo que
-el usuario ingresa en el input "fraseInicial", y que antes de cualquier cosa, lo devuelva en mayúsculas.
+/* le decimos al botón "buttonCode" que al hacer click guarde en la variable "encrypText" lo que
+el usuario ingresa en el input "firstText", y que antes de cualquier cosa, lo devuelva en mayúsculas.
 Luego llamamos a la variable offset para que almacene el desplazamiento que escoje el usuario,
-para finalmente llamar al input de "descifrado" nos devuelva el resultado de la función
+para finalmente llamar al input de "result-text" nos devuelva el resultado de la función
 que le indicamos en el cipher.js, en el bloque de encode */
 
-document.getElementById("btncode").addEventListener("click", () => {
-    let encryptText = (document.getElementById("fraseInicial").value).toUpperCase();
+document.getElementById("buttonCode").addEventListener("click", () => {
+    let encryptText = (document.getElementById("firstText").value).toUpperCase();
     let offset = document.getElementById("key").value;
     offset = parseInt(offset);
-    document.getElementById("descifrado").innerHTML = (cipher.encode(encryptText, offset));
-
+    document.getElementById("resultText").innerHTML = (cipher.encode(encryptText, offset));
 })
  
-/* le decimos al botón "decodificar" que al hacer click guarde en la variable "encrypText" lo que
-el usuario ingresa en el input "fraseInicial", y que antes de cualquier cosa, lo devuelva en mayúsculas.
+/* le decimos al botón "buttonUncode" que al hacer click guarde en la variable "encrypText" lo que
+el usuario ingresa en el input "firstText", y que antes de cualquier cosa, lo devuelva en mayúsculas.
 Luego llamamos a la variable offset para que almacene el desplazamiento que escoje el usuario,
-para finalmente llamar al input de "descifrado" nos devuelva el resultado de la función
+para finalmente llamar al input de "result-text" nos devuelva el resultado de la función
 que le indicamos en el cipher.js, en el bloque de decode */
 
-document.getElementById("btnuncode").addEventListener("click", () => {
-
-    let encryptText = (document.getElementById("fraseInicial").value).toUpperCase();
+document.getElementById("buttonUncode").addEventListener("click", () => {
+    let encryptText = (document.getElementById("firstText").value).toUpperCase();
     let offset = document.getElementById("key").value;
     offset = parseInt(offset);
-    document.getElementById("descifrado").innerHTML = cipher.decode(encryptText, offset);
+    document.getElementById("resultText").innerHTML = cipher.decode(encryptText, offset);
+})
 
+document.getElementById("buttonReset").addEventListener("click", () => {
+    document.getElementById("firstText").value = "";
+    document.getElementById("key").value = "";
+    document.getElementById("resultText").value = "";
 })
